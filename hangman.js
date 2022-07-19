335ea21b8616ff8ab9949ac9e067cc5aafcc933e
Jakub Nowak <kuba9818@gmail.com>
2022-07-19 21:45:33 +0200

    add categories and data

--- hangman.js
+++ hangman.js
@@ -1,16 +1,27 @@
 //data
-var catchphraseArr = new Array();
-catchphraseArr.push("Love is blind", "Practise makes perfect", "East or west, home is best", "There is no place", "Easy come, easy go",
+var categoryArr = [];
+var proverbsArr = [];
+var fruitsArr = [];
+var jobsArr = [];
+var moviesArr = [];
+proverbsArr.push("Love is blind", "Practise makes perfect", "East or west, home is best", "There is no place", "Easy come, easy go",
                     "Where there is a will, there is a way", "Better late than never", "Time heals all wounds",
                     "Time heals all wounds", "Time is money", "All is well that ends well",
                     "Every man has his faults", "Never judge a book by its cover", "Chill, relax, unwind",
                     "All roads lead to Rome", "Every country has its customs", "The early bird catches the worm", "Easier said than done");
+fruitsArr.push("Apple", "Apricot", "Avocado", "Blackcurrant", "Blackberry", "Blueberry", "Coconut", "Fig", "Grape", "Lemon", "Lime", "Lychee",
+                "Nectarine", "Orange", "Papaya", "Peach", "Pear", "Pineapple", "Quince", "Raspberry", "Strawberry");
+jobsArr.push("Teacher", "Soldier", "Scientist", "Waiter", "Waitress", "Worker", "Painter", "Policeman", "Cleaner", "Cook", "Doctor", "Driver", "Engineer",
+            "Explorer", "Farmer", "Fireman", "Footballer", "Hairdresser", "Housemaid", "Judge", "Lawyer", "Musician", "News reporter",
+            "Nurse", "Director", "Photographer", "Pilot", "Politician", "Postman", "Receptionist", "Repairman", "Sailor", "Tour guide");
+moviesArr.push("The Shawshank Redemption", "The Godfather", "The Dark Night", "Pulp Fiction", "Forrest Gump", "Fight CLub", "Inception", "The Matrix",
+                "Goodfellas", "City of God", "Star Wars", "Terminator", "The Pianist", "Parasite", "Gladiator", "The Prestige", "Whiplash");
+categoryArr.push(proverbsArr, fruitsArr, jobsArr, moviesArr);
 
 
-var catchphrase = catchphraseArr[Math.floor(Math.random()*catchphraseArr.length)];
+var catchphrase = proverbsArr[Math.floor(Math.random()*proverbsArr.length)];
 catchphrase = catchphrase.toUpperCase();
 var errors = 0;
-
 var covered_catchphrase = "";
 for (let i=0; i<catchphrase.length; i++){
     if (catchphrase.charAt(i) === " ") covered_catchphrase += " ";
@@ -19,59 +30,78 @@ for (let i=0; i<catchphrase.length; i++){
 function write_catchphrase(){
     document.getElementById("board").innerHTML = covered_catchphrase;
 }
-
-window.onload = start;
-function start(){
-    let content_div = "";
-    for(let i=0; i < 26; i++){
-        content_div += '<div class="letter" id="lett'+ i +'" onclick="check('+i+')">'+String.fromCharCode(65+i)+'</div>';
+function write_new_catchphrase(nr) {
+    catchphrase = categoryArr[nr][Math.floor(Math.random() * categoryArr[nr].length)].toUpperCase();
+    covered_catchphrase ="";
+    for (let i = 0; i < catchphrase.length; i++) {
+        if (catchphrase.charAt(i) === " ") covered_catchphrase += " ";
+        else covered_catchphrase += "_";
     }
-    document.getElementById("alphabet").innerHTML = content_div;
-    write_catchphrase();
-}
-String.prototype.setChar = function (position, char){
-    if(position > this.length -1) return this.toString();
-    else return this.substr(0, position) + char + this.substr(position + 1);
-}
+        document.getElementById("board").innerHTML = covered_catchphrase;
+    }
 
-function check(nr){
-    let tmp = String.fromCharCode(65+nr);
-    let flag = false;
-    for (let i = 0; i<catchphrase.length; i++){
-        if(catchphrase.charAt(i) === tmp){
-            covered_catchphrase = covered_catchphrase.setChar(i, tmp);
-            flag = true;
+    function write_category() {
+        let catchphrases_div = '<div class="category" id="cat0" onclick="write_new_catchphrase(0)">catchphrases</div>';
+        let fruits_div = '<div class="category" id="cat1" onclick="write_new_catchphrase(1)">fruits</div>';
+        let jobs_div = '<div class="category" id="cat2" onclick="write_new_catchphrase(2)">jobs</div>';
+        let movies_div = '<div class="category" id="cat3" onclick="write_new_catchphrase(3)">movies</div>';
+        let categories_divs = catchphrases_div + fruits_div + jobs_div + movies_div;
+        document.getElementById("board").innerHTML = '<div class ="categories">' + categories_divs + '</div>';
+    }
+
+    window.onload = start;
+
+    function start() {
+        let content_div = "";
+        for (let i = 0; i < 26; i++) {
+            content_div += '<div class="letter" id="lett' + i + '" onclick="check(' + i + ')">' + String.fromCharCode(65 + i) + '</div>';
         }
-    }
-    let element = "lett" + nr;
-    if (flag){
-        document.getElementById(element).style.background = "#003300";
-        document.getElementById(element).style.color = "#00C000";
-        document.getElementById(element).style.border = "3px solid #00C000";
-        document.getElementById(element).style.cursor = "default";
+        document.getElementById("alphabet").innerHTML = content_div;
         write_catchphrase();
     }
-    else {
-        document.getElementById(element).style.background = "#7e2020";
-        document.getElementById(element).style.color = "#fe4040";
-        document.getElementById(element).style.border = "3px solid #fe4040";
-        document.getElementById(element).style.cursor = "default";
-        document.getElementById(element).setAttribute("onclick",";");
 
-        changeImg();
-    }
-    //win
-    if (catchphrase == covered_catchphrase){
-        document.getElementById("alphabet").innerHTML = "You win! The answer was: " + catchphrase + '<br/><br/><span class="reset" onclick="location.reload()">Play again?</span>';
-    }
-    //loose
-    if (errors >= 9){
-        document.getElementById("alphabet").innerHTML = "You loose! The answer was: " + catchphrase + '<br/><br/><span class="reset" onclick="location.reload()">Play again?</span>';
+    String.prototype.setChar = function (position, char) {
+        if (position > this.length - 1) return this.toString();
+        else return this.substr(0, position) + char + this.substr(position + 1);
     }
 
-    function changeImg(){
-        let image = "img/s" + (++errors) + ".jpg";
-        document.getElementById("gibbet").innerHTML = '<img src="'+image+'" alt="" />';
-        setTimeout("changeImg()", 50000);
-    }
+    function check(nr) {
+        let tmp = String.fromCharCode(65 + nr);
+        let flag = false;
+        for (let i = 0; i < catchphrase.length; i++) {
+            if (catchphrase.charAt(i) === tmp) {
+                covered_catchphrase = covered_catchphrase.setChar(i, tmp);
+                flag = true;
+            }
+        }
+        let element = "lett" + nr;
+        if (flag) {
+            document.getElementById(element).style.background = "#003300";
+            document.getElementById(element).style.color = "#00C000";
+            document.getElementById(element).style.border = "3px solid #00C000";
+            document.getElementById(element).style.cursor = "default";
+            write_catchphrase();
+        } else {
+            document.getElementById(element).style.background = "#7e2020";
+            document.getElementById(element).style.color = "#fe4040";
+            document.getElementById(element).style.border = "3px solid #fe4040";
+            document.getElementById(element).style.cursor = "default";
+            document.getElementById(element).setAttribute("onclick", ";");
+
+            changeImg();
+        }
+        //win
+        if (catchphrase == covered_catchphrase) {
+            document.getElementById("alphabet").innerHTML = "You win! <br/>The answer was:</br> " + catchphrase + '<br/><br/><span class="reset" onclick="location.reload()">Play again?</span>';
+        }
+        //loose
+        if (errors >= 9) {
+            document.getElementById("alphabet").innerHTML = "You loose! <br/>The answer was:<br/> " + catchphrase + '<br/><br/><span class="reset" onclick="location.reload()">Play again?</span>';
+        }
+
+        function changeImg() {
+            let image = "img/s" + (++errors) + ".jpg";
+            document.getElementById("gibbet").innerHTML = '<img src="' + image + '" alt="" />';
+            setTimeout("changeImg()", 50000);
+        }
 }
